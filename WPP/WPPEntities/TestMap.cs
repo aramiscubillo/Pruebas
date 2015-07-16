601cd9ae7723c168461b69c5439923c878b3c50d
aramiscubillo <acubillo@sapiens.co.cr>
2015-07-16 08:14:03 -0600

    Initial proyecto commit

---
+++ WPP/WPPEntities/TestMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPPEntities
+{
+    public class TestMap 
+    {
+        /*public TestMap()
+        {
+            IDictionary()
+        }*/
+    }
+}
