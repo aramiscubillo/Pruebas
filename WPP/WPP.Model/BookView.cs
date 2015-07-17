0fec08cad5393430f1b7add68e14649df06809f9
dbarboza <dbarboza@sapiens.co.cr>
2015-07-17 14:02:40 -0600

    Nunit, FakeItEasy and AutoMapper Configuration

---
+++ WPP/WPP.Model/BookView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPP.Model
+{
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+}
--- WPP/WPP/Global.asax.cs
+++ WPP/WPP/Global.asax.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WPP.Entities;
+
 
 namespace WPP
 {
@@ -15,10 +18,10 @@ namespace WPP
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
         }
     }
 }
