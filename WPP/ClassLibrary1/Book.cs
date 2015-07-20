3b6262d3e019519afcf5c24d2162079829e3afb6
dbarboza <dbarboza@sapiens.co.cr>
2015-07-20 10:47:33 -0600

    Add Book

---
+++ WPP/ClassLibrary1/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPP.Entities
+{
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
